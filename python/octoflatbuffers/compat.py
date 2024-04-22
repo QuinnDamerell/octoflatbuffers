09cb02bc3ddd4d6f990b21fc186460cb97ec6c8c
Quinn Damerell <quinnd@outlook.com>
2024-04-22 08:25:42 -0700

    Removing numpy loading, since it breaks things.

--- python/octoflatbuffers/compat.py
+++ python/octoflatbuffers/compat.py
@@ -53,26 +53,31 @@ def import_numpy():
     Returns the numpy module if it exists on the system,
     otherwise returns None.
     """
-    if PY3:
-        numpy_exists = (
-            importlib.machinery.PathFinder.find_spec('numpy') is not None)
-    else:
-        try:
-            imp.find_module('numpy')
-            numpy_exists = True
-        except ImportError:
-            numpy_exists = False
+    # if PY3:
+    #     numpy_exists = (
+    #         importlib.machinery.PathFinder.find_spec('numpy') is not None)
+    # else:
+    #     try:
+    #         imp.find_module('numpy')
+    #         numpy_exists = True
+    #     except ImportError:
+    #         numpy_exists = False
 
-    if numpy_exists:
-        # We do this outside of try/except block in case numpy exists
-        # but is not installed correctly. We do not want to catch an
-        # incorrect installation which would manifest as an
-        # ImportError.
-        import numpy as np
-    else:
-        np = None
-
-    return np
+    # if numpy_exists:
+    #     # We do this outside of try/except block in case numpy exists
+    #     # but is not installed correctly. We do not want to catch an
+    #     # incorrect installation which would manifest as an
+    #     # ImportError.
+    #     import numpy as np
+    # else:
+    #     np = None
+    #
+    # return np
+    # OctoEverywhere change! - Disable any type of numpy loading.
+    # We found a few of our users had other plugins attempt to load numpy and it didn't fully install.
+    # Thus, when numpy attempts to load, it discovers it's broken, and throws and fails, and messes up our plugin.
+    numpy_exists = False
+    return None
 
 
 class NumpyRequiredForThisFeature(RuntimeError):
